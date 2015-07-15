8e3027ff1174a9bcc236bcebb7fdd5b797d4514e
John Karahalis <john.karahalis@gmail.com>
2015-07-15 14:17:10 -0400

    Remove bodyParser

--- app.js
+++ app.js
@@ -2,17 +2,15 @@
 
 var path = require('path');
 
+var express = require('express');
+
 var hook = require('./hook');
 var config = require('./config');
 var logger = require('./logger');
 
-// Set up Express
-var express = require('express');
-var bodyParser = require('body-parser');
 var port = process.env.PORT || config.port;
 
 var app = express();
-app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'build/www')));
 app.post('/hook', hook.handle);
 app.listen(port);
